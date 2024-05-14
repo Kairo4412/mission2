3f1d90917f527f7a53494488e907ce4f8d32463a
kairo4412 <kairo4412@gmail.com>
2024-05-14 11:30:37 +0900

    for문

--- src/main/java/mission/Mission.java
+++ src/main/java/mission/Mission.java
@@ -3,8 +3,11 @@ package mission;
 public class Mission {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		for(int i = 1; i<11; i++) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
