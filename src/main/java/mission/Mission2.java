de167aa72169e7540aba70de06c0c6e5f48bb92c
kairo4412 <kairo4412@gmail.com>
2024-05-14 11:34:27 +0900

    sum

---
+++ src/main/java/mission/Mission2.java
@@ -0,0 +1,14 @@
+package mission;
+
+public class Mission2 {
+
+	public static void main(String[] args) {
+		
+		int num = 0;
+		for(int i = 1; i < 101; i++) {
+			num += i;
+		}
+		System.out.println(num);
+	}
+
+}
